9607567ea2d9fc18df4884d092bf9ef41e1350b5
Sebastian Schmidt <yath@yath.de>
2009-08-10 22:37:43 +0200

    export str_is_utf8()

--- src/core/recode.h
+++ src/core/recode.h
@@ -5,6 +5,7 @@ char *recode_in (const SERVER_REC *server, const char *str, const char *target);
 char *recode_out (const SERVER_REC *server, const char *str, const char *target);
 gboolean is_valid_charset(const char *charset);
 gboolean is_utf8(void);
+gboolean str_is_utf8(const char *, size_t);
 void recode_update_charset(void);
 
 void recode_init (void);
